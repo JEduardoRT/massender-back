1291a240805c36a53f8632e520a0e57cabfc684b
maryta22 <maria.mariaog.rivera@gmail.com>
2024-08-13 10:07:42 -0500

    envio sms

--- services/send_sms.py
+++ services/send_sms.py
@@ -1,4 +1,35 @@
+import requests
+import json
+import logging
+
+# Configurar el registro
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
-def enviar_sms(phone_list, mensaje):
-    pass
+def enviar_sms_masivo(numeros_telefonos, nombre_campania):
+
+    for numero_telefono in numeros_telefonos:
+        url = "https://api.massend.com/api/sms"
+
+        # Construcción del payload
+        payload = json.dumps({
+            "user": "Hangaroa@massend.com",
+            "pass": "Hangaroa123@#",
+            "mensajeid": "43773",
+            "campana": nombre_campania,
+            "tipo": 0,
+            "ruta": 0,
+            "telefono": numero_telefono
+        })
+
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+        logger.info(response)
+
+        return response.text
+
+
